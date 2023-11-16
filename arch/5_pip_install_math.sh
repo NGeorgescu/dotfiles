2dc45937df372ecfd815b0bcc2b20798d8ac76cd
Nicholas S Georgescu <nsgeorgescu@gmail.com>
2023-11-16 18:30:53 -0500

    fixed pip script typo

--- arch/5_pip_install_math.sh
+++ arch/5_pip_install_math.sh
@@ -2,7 +2,7 @@
 
 export WORKON_HOME=~/.virtualenvs
 source /usr/bin/virtualenvwrapper.sh
-mkcirtualenv math
+mkvirtualenv math
 workon math
 pip install -U numpy
 pip install -U torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
